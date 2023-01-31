719977338f87d39d0677839cfe1ca1e4967ae4dd
Rallefar1103 <rasmus@turftank.com>
2023-01-31 10:33:54 -0800

    remove non executable files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,8 +4,9 @@ module org.jfree.demos {
     requires org.jfree.svg;
     requires org.jfree.pdf;
     requires org.jfree.jfreechart;
-    requires org.jfree.chart3d;
+    // requires org.jfree.chart3d;
     requires com.formdev.flatlaf;
+
     exports com.orsoncharts.demo;
     exports org.jfree.pdf.demo;
     exports org.jfree.chart.demo2;
